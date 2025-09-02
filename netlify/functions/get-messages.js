c20967a4cc5d2bb2da42022156b794a0b0afcb2b
Adefabi23 <adefabijunaidolamilekan@2019>
2025-09-02 20:18:47 +1300

    Update get-messages function with better logging

--- netlify/functions/get-messages.js
+++ netlify/functions/get-messages.js
@@ -12,12 +12,12 @@ exports.handler = async (event, context) => {
   // Set up default messages
   const defaultMessages = [
     {
-      author: "Grandma",
+      author: "Grandpa",
       date: "Today",
       content: "Happy first birthday to our precious grandchildren! Watching you both grow this year has been the greatest joy! We love you more than words can say!"
     },
     {
-      author: "Aunt Sarah",
+      author: "Grandma",
       date: "Today",
       content: "Happy birthday to the two most adorable twins! Armani and Ameerah, you bring so much happiness to our family. Can't wait to celebrate with you!"
     }
